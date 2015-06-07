17696681748659c253d398f397e9bc7a665b56e0
Daniela Baron <baron.daniela@gmail.com>
2015-06-07 19:18:49 -0400

    slight refactoring for problem 4

--- test/lib/numArrangerSpec.js
+++ test/lib/numArrangerSpec.js
@@ -5,15 +5,15 @@ var expect = require('chai').expect;
 
 describe('numArranger', function() {
 
-  it('Example 1', function() {
+  it('Largest arrangement of [50, 2, 1, 9] is 95021', function() {
     expect(numArranger.arrange([50, 2, 1, 9])).to.equal(95021);
   });
 
-  it('Example 2', function() {
+  it('Largest arrangement of [5, 50, 56] is 56550', function() {
     expect(numArranger.arrange([5, 50, 56])).to.equal(56550);
   });
 
-  it('Example 3', function() {
+  it('Largest arrangement of [420, 42, 423] is 42423420', function() {
     expect(numArranger.arrange([420, 42, 423])).to.equal(42423420);
   });
 
